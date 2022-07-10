9c83ee7ebe210034e61df3a0622a4641c27ba2e0
fuseSavage <chaiwat.si.61@ubu.ac.th>
2022-07-10 17:56:19 +0700

    covid

---
+++ js/about.8c68399d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkglobal_covid_19"]=self["webpackChunkglobal_covid_19"]||[]).push([[443],{399:function(n,t,u){u.r(t),u.d(t,{default:function(){return c}});var l=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],a=u(1),e={},i=(0,a.Z)(e,l,s,!1,null,null,null),c=i.exports}}]);
+//# sourceMappingURL=about.8c68399d.js.map
