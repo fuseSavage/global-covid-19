267ab48ba20c3abc52eed05ca2f73d4b877e9a77
fuseSavage <chaiwat.si.61@ubu.ac.th>
2022-07-10 17:40:36 +0700

    covid-19

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import report from './report'
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    report
+  }
+})
